2d47b7b69e647cc607cd70f1cfb10cf435e74d8f
Jéssika Sousa <jessika_jps@hotmail.com>
2022-08-14 19:03:23 -0300

    fix: json response

--- Controllers/GitHubApiController.cs
+++ Controllers/GitHubApiController.cs
@@ -1,18 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
 using System.Text.Json;
-
+using System.Threading.Tasks;
 namespace api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class GitHubApiController : ControllerBase
+    public class GithubController : ControllerBase
     {
-        [HttpGet]
-         public async Task<IEnumerable<GitHubApiModel>> GetRepositoriesGithub(string profile, int pageNumber, string language, string repositoryName, DateTime createdAt)
-        {
 
+        [HttpGet]
+        public async Task<IEnumerable<GitHubApiModel>> GetRepositoriesGithub(string profile, int pageNumber, string repositoryName, DateTime createdAt)
+        {
             var request = new HttpRequestMessage(HttpMethod.Get, "https://api.github.com/users/" + profile + "/repos?" + "page=" + pageNumber);
-            request.Headers.Add("Accept", "application/vnd.github.v6+json");
+            request.Headers.Add("Accept", "application/vnd.github.v3+json");
             request.Headers.Add("User-Agent", "jessikasousa");
 
             var client = new HttpClient();
@@ -23,9 +27,6 @@ namespace api.Controllers
                 var responseStream = await response.Content.ReadAsStreamAsync();
                 var list = await JsonSerializer.DeserializeAsync
                     <IEnumerable<GitHubApiModel>>(responseStream);
-
-                if (!String.IsNullOrEmpty(language))
-                    list = list.Where(x => x.Language == language);
 
                 if (createdAt > DateTime.MinValue)
                     list = list.Where(x => x.CreatedAt.Substring(0, 10) == createdAt.ToString("yyy-MM-dd").Substring(0, 10));
