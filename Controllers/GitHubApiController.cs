d14906b967bcb88e829c686bcd3b73f7def32f99
Jéssika Sousa <jessika_jps@hotmail.com>
2022-08-14 10:26:52 -0300

    fix: header

--- Controllers/GitHubApiController.cs
+++ Controllers/GitHubApiController.cs
@@ -12,7 +12,7 @@ namespace api.Controllers
         {
 
             var request = new HttpRequestMessage(HttpMethod.Get, "https://api.github.com/users/" + profile + "/repos?" + "page=" + pageNumber);
-            request.Headers.Add("accept", "application/vnd.github+json");
+            request.Headers.Add("Accept", "application/vnd.github.v4+json");
             request.Headers.Add("User-Agent", "jessikasousa");
 
             var client = new HttpClient();
